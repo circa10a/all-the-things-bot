1e6debd5235c4e1811a94fe63540eabbe41d9d8b
Caleb Lemoine <caleblemoine@gmail.com>
2018-12-30 16:51:42 -0600

    add express route to bootleg uptime robot

--- index.js
+++ index.js
@@ -1,28 +1,37 @@
 const schedule = require('node-schedule');
 const snoowrap = require('snoowrap');
+const express = require('express');
+const app = express();
+const { PORT } = process.env;
 const { checkEnvVars } = require('./lib/check-env-vars');
 const { jobs } = require('./config/jobs');
-const { CLIENT_ID, CLIENT_SECRET, USERNAME, PASSWORD } = process.env;
+
+const {
+  CLIENT_ID, CLIENT_SECRET, USERNAME, PASSWORD,
+} = process.env;
 
 // Ensure all secrets are present
-checkEnvVars({ CLIENT_ID, CLIENT_SECRET, USERNAME, PASSWORD });
+checkEnvVars({
+  CLIENT_ID, CLIENT_SECRET, USERNAME, PASSWORD,
+});
 
 // Construct new connection
- const r = new snoowrap({
-    userAgent: 'halloween-bot',
-    clientId: CLIENT_ID,
-    clientSecret: CLIENT_SECRET,
-    username: USERNAME,
-    password: PASSWORD
-  });
+const r = new snoowrap({
+  userAgent: 'halloween-bot',
+  clientId: CLIENT_ID,
+  clientSecret: CLIENT_SECRET,
+  username: USERNAME,
+  password: PASSWORD,
+});
 
 // Create jobs
 jobs.forEach((job) => {
   schedule.scheduleJob(job.schedule, () => {
     // Post to halloween subreddit
-     r.getSubreddit('testingground4bots')
-       .submitSelfpost({title: job.title, text: job.text})
+    r.getSubreddit('testingground4bots')
+      .submitSelfpost({ title: job.title, text: job.text });
   });
 });
 
-console.log('Halloween Reddit Bot started...')
+app.get('/', (req, res) => res.send('Halloween Reddit Bot started...'));
+app.listen(PORT || 8000, () => console.log(`App listening on port ${port}!`));
